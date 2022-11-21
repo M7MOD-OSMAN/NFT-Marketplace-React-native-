9e009a33850239ff2eedc601d31e7b06217f2bf3
M7MOD-OSMAN <M7MOD-OSMAN@github.com>
2022-11-21 04:47:17 +0200

    initial commit

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import HomeHeader from './HomeHeader'
+import NFTCard from './NFTCard'
+import { CircleButton, RectButton } from './Button'
+import FocusedStatusBar from './FocusedStatusBar'
+import DetailsBid from './DetailsBid'
+import DetailsDesc from './DetailsDesc'
+import { SubInfo } from './SubInfo'
+
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  DetailsBid,
+  DetailsDesc,
+  SubInfo,
+}
